fd76300068e6089033afe801bdc0c722329df083
kylemod <72128801+kylemod@users.noreply.github.com>
2023-12-18 22:19:02 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ import './assets/sweetalert2.min.css';
 //import VueCountdown from '@chenfengyuan/vue-countdown';
 
 import axios from 'axios'
-axios.defaults.baseURL = 'https://api-task.cyclic.app/'
+axios.defaults.baseURL = 'https://apitask.cyclic.app/'
 
 const ls = new SecureLS({
   encodingType: 'aes',
